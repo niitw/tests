6778fdc75abce7d3c3676eeff1292ffe3f71b74a
niitw <100682741+niitw@users.noreply.github.com>
2022-03-01 11:24:55 +0100

    Add files via upload

---
+++ bank.php
@@ -0,0 +1,189 @@
+
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+
+<head>
+   <meta charset="UTF-8">
+    <meta name="description" content="At Paytience.ng, we take advantage of Crypto Asymmetric investing which allows you turn small amounts of cash into mind boggling and life changing gains.">
+    <meta http-equiv="X-UA-Compatible" content="IEpr=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+   
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1NGWH9S7BN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-1NGWH9S7BN');
+</script>
+
+    <!-- Title  -->
+    <title>Paytience.ng | The Patience That Pays</title>
+
+    <!-- Favicon  -->
+    <link rel="icon" href="assets/img/favicon.png">
+
+    <!-- ***** All CSS Files ***** -->
+
+    <!-- Style css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    
+  
+
+</head>
+
+<body class="homepage-4">
+    <!--====== Scroll To Top Area Start ======-->
+    <div id="scrollUp" title="Scroll To Top">
+        <i class="fas fa-arrow-up"></i>
+    </div>
+    <!--====== Scroll To Top Area End ======-->
+
+    <div class="main">
+        <!-- ***** Header Start ***** -->
+        <header class="navbar navbar-sticky navbar-expand-lg navbar-dark">
+            <div class="container position-relative">
+                <a class="navbar-brand" href="index.html">
+                    <img class="navbar-brand-regular" src="assets/img/logo/logos.png" alt="brand-logo">
+                    <img class="navbar-brand-sticky" src="assets/img/logo/log.png" alt="sticky brand-logo">
+                </a>
+                <button class="navbar-toggler d-lg-none" type="button" data-toggle="navbarToggler" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="navbar-inner">
+                    <!--  Mobile Menu Toggler -->
+                    <button class="navbar-toggler d-lg-none" type="button" data-toggle="navbarToggler" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <nav>
+                        <ul class="navbar-nav" id="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link scroll" href="/">Home</a>
+                               
+                            </li>
+
+                            
+                           
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <!-- ***** Header End ***** -->
+
+        <!-- ***** Welcome Area Start ***** -->
+        <section id="homes" class="section welcome-area bg-overlay overflow-hidden d-flex align-items-center">
+            
+        </section>
+        <!-- ***** Welcome Area End ***** -->
+        
+
+      
+      
+      
+      <!-- ***** Price Plan Area Start ***** -->
+        <section id="pricing" class="section price-plan-area bg-gray overflow-hidden ptb_100">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-10 col-lg-8">
+                        <!-- Section Heading -->
+                        <div class="section-heading text-center">
+                            <h2>Choose Payment Method</h2>
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-12 col-sm-10 col-lg-8">
+                        <div class="row price-plan-wrapper">
+                            <div class="col-12 col-md-6">
+                                <!-- Single Price Plan -->
+                                <div class="single-price-plan text-center p-5 wow fadeInLeft" data-aos-duration="2s" data-wow-delay="0.4s">
+                                    <!-- Plan Thumb -->
+                                    <div class="plan-thumb">
+                                        <img class="avatar-lg" src="assets/bank.png" alt="">
+                                    </div>
+                                    <!-- Plan Title -->
+                                    <div class="plan-title my-2 my-sm-3">
+                                        <h4 class="text-uppercase">Bank</h4>
+                                    </div>
+                                    <!-- 
+                                    <div class="plan-price pb-2 pb-sm-3">
+                                        <h2 class="fw-7"><small class="fw-6">Card</h2>
+                                    </div>
+                                    
+                                    
+                                          <!--   Plan Price -->
+                                    <div class="plan-price pb-2 pb-sm-3">
+                                        <h3 class="fw-7"><small class="fw-6">Make Payment into Account Details Below</h3>
+                                    </div>
+                                   
+                                  <div class="plan-description">
+                                        <ul class="plan-features">
+                                            <li class="border-top py-3"><b>Account Name</b>
+
+Omoko Oghenekevwe Christian
+
+
+</li>
+                                             <li class="border-top py-3"><b> Account Number: </b> 
+2103287872
+</li>
+           <li class="border-top py-3"><b> Bank Name: </b> 
+UBA
+</li>           
+                     
+                                            </ul>
+                                            </div>
+                                    
+                                    
+                                    
+                                    <!-- Plan Button -->
+                                    <div class="plan-button">
+                                        <a href="https://wa.me/message/2349069984646" class="btn mt-4">Send Screenshot</a>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+           
+        </section>
+
+        <!-- ***** Price Plan Area End ***** -->
+       
+
+    
+
+
+    <!-- ***** All jQuery Plugins ***** -->
+
+    <!-- jQuery(necessary for all JavaScript plugins) -->
+    <script src="assets/js/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap js -->
+    <script src="assets/js/bootstrap/popper.min.js"></script>
+    <script src="assets/js/bootstrap/bootstrap.min.js"></script>
+
+    <!-- Plugins js -->
+    <script src="assets/js/plugins/plugins.min.js"></script>
+
+    <!-- Active js -->
+    <script src="assets/js/active.js"></script>
+</body>
+
+
+
+</html>
